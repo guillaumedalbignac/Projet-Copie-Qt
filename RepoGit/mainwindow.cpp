7a139cd4328f67a95bee7ddb7c0370ac096384f9
guillaumedalbignac <dalbignacguillaume@gmail;com>
2020-01-08 14:31:10 +0100

    App test d'affichage d'images

--- RepoGit/mainwindow.cpp
+++ RepoGit/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
+#include <QDir>
+#include <QFileInfo>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QDir monRepertoire("C:/User/dalbi/Desktop/ProjetBTS/RepoGit");
+    foreach(QFileInfo var, monRepertoire.entryInfoList()){
+        ui->comboBox->addItem(var.absoluteFilePath());
+
+    }
 }
 
 MainWindow::~MainWindow()
@@ -13,8 +21,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
 {
-ui->label->setText("bonjour");
+    QPixmap photos = ui->comboBox->currentText();
+    ui->label_2->setPixmap(photos.scaled(779,443, Qt::KeepAspectRatioByExpanding));
 }
